02aea9270ca80cf0b38f3f997fd453c9d500268b
albertsp <albertsanchez98@gmail.com>
2026-05-27 01:16:21 +0200

    feat: auth context + auth provider

---
+++ frontend/src/context/AuthContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext()
+
+export function AuthProvider({children}){
+
+    const [user, setUser] = useState("")
+    const [token, setToken] = useState("")
+
+    function login(logUser,logToken){
+        setToken(logToken)
+        setUser(logUser)
+    }
+    function logout(){
+        setToken("")
+        setUser("")
+    }
+
+    return(
+        <AuthContext.Provider value={{token, user, login, logout}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export function useAuth(){
+    return useContext(AuthContext)
+}
+
